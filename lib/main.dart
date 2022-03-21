380964089494c8fc2614fbb9bd54fbd363371d02
Piotr Orzechowski <piotrekorzech7@gmail.com>
2022-03-21 18:19:41 +0100

    Created modal sheet of adding new transaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/widgets/transaction_list.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-import './widgets/user_transactions.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,11 +22,53 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   // String titleInput; //input values are always Strings by default
-  // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 'tx1',
+      title: 'Odżywka białkowa',
+      amount: 42.50,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 'tx2',
+      title: 'Kreatyna',
+      amount: 27.50,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String transactionTitle, double transactionAmount) {
+    final newTransaction = Transaction(
+      title: transactionTitle,
+      amount: transactionAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+    // it's final because here I create it with the values that are passed into this method so it is created based on value which i don't know at the point of time I'm writing the code , hence const can't be used here but I can use final because once that transaction has been created with these dynamic values, it will not change again
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        //showModalBottomSheet needs at least two arguments the first is a value for context because internally, this function will use this strange context object to show that modal sheet, so we need to pass a value for context here, so we accept it as an argument
+        // then we also need a builder. Builder now is a function that needs to return the widget that should be inside of that modal bottom sheet, the builder function itself also give us a context ((_))
+        // inside builder function you now return widget you want to show inside of that modal sheet
+        return NewTransaction(_addNewTransaction);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +79,8 @@ class MyHomePage extends StatelessWidget {
         title: Text('Kalkulator wydatków'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _startAddNewTransaction(
+                context), //we need to forward there context   Widget build(BuildContext context) - which is this context value here,
             icon: Icon(Icons.add),
           )
         ], //actions argument takes a list of widgets, theoretically you can cadd any widget in ehre, like text or whatever you want, most typically you add icon buttons here
@@ -51,14 +97,15 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions(),
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(
+            context), //we need to forward there context   Widget build(BuildContext context) - which is this context value here
       ),
     );
   }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final Function addTransaction; //hold a pointer at a function
+class NewTransaction extends StatefulWidget {
+  final Function addTransaction;
+  NewTransaction(this.addTransaction);
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  //hold a pointer at a function
   final titleController = TextEditingController();
+
   final amountController = TextEditingController();
 
-  NewTransaction(
-      this.addTransaction); //add new transaction and bind whatever you get to addTransaction property
-
+  //add new transaction and bind whatever you get to addTransaction property
   void submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
@@ -15,10 +21,16 @@ class NewTransaction extends StatelessWidget {
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       return; //return stops the function execution, it means code after return won't be reached
     }
-    addTransaction(
+    widget.addTransaction(
+      // thanks to widget. i can access poperty addTransaction which has this function reference i get even though i'm tehnically in different class
       enteredTitle,
       enteredAmount,
     );
+    Navigator.of(context).pop();
+    //closing the topmost screen that is displayed and that is that modal sheet if it's opened
+    //.of(context) here is required to get access to the right navigator or to give the navigator some meta data
+    //and context here is a special property which is available class wide in state class here even though you never defined a property named context
+    // it's made available because we extend state
   }
 
   @override
