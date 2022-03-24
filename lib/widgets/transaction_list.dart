315d48af65068a1487ff2e81df666348de445d34
Piotr Orzechowski <piotrekorzech7@gmail.com>
2022-03-24 11:28:09 +0100

    Transaction List new styling

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 400,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -42,55 +42,84 @@ class TransactionList extends StatelessWidget {
                 //the value of context(it can be any name) well be of type BuildContext, this function is not called by me so it's not my job to provide these arguments, it's called by Flutter because it calls this builder function here  for every new item it wants to render on the screen
                 // and it gives us a context which we don't need here but still we get it and it gives us also and index of that item so is it the first item, second item, third.... of the items it should render
                 return Card(
-                  // in here we need to return a widget inside this builder method, that i built for this item we're currently looking at  so a widget that is build for the first item, for the second item and so on
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(10),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2,
-                          ),
-                        ),
-                        child: Text(
-                          '${transactions[index].amount.toStringAsFixed(2)} zł',
-                          //interpolacja stringa, pewne miejsce w stringu zostaje zastąpione wartością za pomocą znaku $ oraz {}, nie musimy dodawac .toString
-                          //.toStringAsFixed(2) special kind of .toString which defines how many decimal places you want to show
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 6),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                              '${transactions[index].amount.toStringAsFixed(2)} zł'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 5),
-                            child: Text(transactions[index].title,
-                                style: Theme.of(context).textTheme.headline6),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 5),
-                            child: Text(
-                              DateFormat('yMMMEd', 'pl')
-                                  .format(transactions[index].date),
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ), //title is element in the middle
+                    subtitle: Text(
+                      DateFormat('yMMMEd', 'pl')
+                          .format(transactions[index].date),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    //trailing:  ,//element at the end of that ListTile
                   ),
                 );
+
+                // return Card(
+                //   // in here we need to return a widget inside this builder method, that i built for this item we're currently looking at  so a widget that is build for the first item, for the second item and so on
+                //   child: Row(
+                //     children: [
+                //       Container(
+                //         padding: EdgeInsets.all(10),
+                //         margin:
+                //             EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                //         decoration: BoxDecoration(
+                //           borderRadius: BorderRadius.all(Radius.circular(10)),
+                //           border: Border.all(
+                //             color: Theme.of(context).colorScheme.primary,
+                //             width: 2,
+                //           ),
+                //         ),
+                //         child: Text(
+                //           '${transactions[index].amount.toStringAsFixed(2)} zł',
+                //           //interpolacja stringa, pewne miejsce w stringu zostaje zastąpione wartością za pomocą znaku $ oraz {}, nie musimy dodawac .toString
+                //           //.toStringAsFixed(2) special kind of .toString which defines how many decimal places you want to show
+                //           style: TextStyle(
+                //             fontWeight: FontWeight.bold,
+                //             fontSize: 24,
+                //             color: Theme.of(context).colorScheme.primary,
+                //           ),
+                //         ),
+                //       ),
+                //       Column(
+                //         crossAxisAlignment: CrossAxisAlignment.start,
+                //         children: [
+                //           Container(
+                //             margin: EdgeInsets.symmetric(vertical: 5),
+                //             child: Text(transactions[index].title,
+                //                 style: Theme.of(context).textTheme.headline6),
+                //           ),
+                //           Container(
+                //             margin: EdgeInsets.symmetric(vertical: 5),
+                //             child: Text(
+                //               DateFormat('yMMMEd', 'pl')
+                //                   .format(transactions[index].date),
+                //               style: TextStyle(
+                //                 fontSize: 14,
+                //                 color: Colors.grey,
+                //               ),
+                //             ),
+                //           )
+                //         ],
+                //       )
+                //     ],
+                //   ),
+                // );
               },
               itemCount: transactions.length,
               // for ListView.builder() we cant provide children thing, instead what we need to provide is the itemCount argument and it defines how many items should be built,
